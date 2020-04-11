128faf5e96838a656b8dd99451e171c5130a003c
Lucas Eduardo <lucased78@hotmail.com>
2020-04-11 14:40:28 -0300

    [BACKEND] Fixing cors issue

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,18 @@
 import express from 'express';
 import PokerRoutes from './routes/PokerRoutes';
+import cors from 'cors';
 
 class Application {
   app = express();
 
   init(){
     this.middlewares();
-    this.app.listen(process.env.PORT || 3000, () => console.log('server started...'))
+    this.app.listen(process.env.PORT || 9000, () => console.log('server started...'))
   }
 
   middlewares(){
     this.app.use(express.json());
+    this.app.use(cors());
     this.app.use('/poker', new PokerRoutes().getRoutes());
   }
 }
