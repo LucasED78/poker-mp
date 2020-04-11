02969b3688d0e8c3b5cc8cef55f4e46620bc4202
Lucas Eduardo <lucased78@hotmail.com>
2020-04-11 11:43:00 -0300

    [FRONTEND] Adding PokerCard component

---
+++ front-react/src/components/PokerCard/PokerCard.tsx
@@ -0,0 +1,44 @@
+import React, { MouseEvent } from 'react';
+import classes from './PokerCard.module.css';
+import ImageUtil from '../../utils/ImageUtil';
+
+
+export interface PokerCardProps {
+  backColor: string;
+  cardName?: string;
+  flip?: boolean;
+}
+
+const PokerCard = (props: PokerCardProps) => {
+  const img = require(`../../assets/card-${props.backColor}.png`).default;
+
+  const onClickHandler = (e: MouseEvent<HTMLImageElement, globalThis.MouseEvent>) => {
+    const { currentTarget: element } = e;
+
+    if (props.flip) {
+      if (!e.currentTarget.style.transform) {
+        element.style.transform = 'rotate(360deg)';
+  
+        if (props.cardName) {
+          const name = ImageUtil.getCardImage(props.cardName);
+          element.src = require(`../../assets/suits/${name}.png`).default
+        }
+        
+        return;
+      }
+  
+      element.style.transform = '';
+      element.src = require(`../../assets/card-${props.backColor}.png`).default
+    }
+  }
+  
+  return (
+    <img 
+      src={img} 
+      className={classes.PokerCard} 
+      alt={`poker card ${props.backColor}`}
+      onClick={onClickHandler} />
+  )
+};
+
+export default PokerCard;
